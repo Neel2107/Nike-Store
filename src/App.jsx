4b128d74117c537da066ed5cfa4101f89e333765
Neel <neelpatel805804@gmail.com>
2023-02-10 22:23:01 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {heroapi} from "./data/data"
+import { Hero } from "./components"
+function App() {
+
+  return (
+    < >
+
+  <Hero heroapi= {heroapi}/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Hero = ({heroapi :{title, subtitle, btntext, img, sociallinks, videos}}) => {
+  return (
+    <>
+      <div className="relative h-auto w-auto flex flex-col">
+        <div></div>
+        <div className="bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10">
+          <div className="">
+            <h1>{title}</h1>
+            <h1>{subtitle}</h1>
+            <button>{btntext}</button>
+            {/* <div></div>
+            <div></div> */}
+            </div>
+            <div className="">
+            <img src={img}
+            className=""
+            alt="hero-img" />
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
