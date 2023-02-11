d2273a9809583586d46bf1edfb79ad6e5ce2c987
Neel <neelpatel805804@gmail.com>
2023-02-11 22:19:46 +0530

    img resize

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ const Hero = ({heroapi :{title, subtitle, btntext, img, sociallinks, videos}}) =
   return (
     <>
       <div className="relative h-auto w-auto flex flex-col">
-        <div></div>
-        <div className="bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10">
-          <div className="">
+        <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-0 z-10'></div>
+        <div  className=" relative opacity-100 z-20 grid items-center justify-items-center nike-container" >
+          <div className="grid items-center justify-items-center mt-28">
             <h1>{title}</h1>
             <h1>{subtitle}</h1>
             <button>{btntext}</button>
@@ -15,7 +15,7 @@ const Hero = ({heroapi :{title, subtitle, btntext, img, sociallinks, videos}}) =
             </div>
             <div className="">
             <img src={img}
-            className=""
+            className="w-[55vh]"
             alt="hero-img" />
             </div>
         </div>
