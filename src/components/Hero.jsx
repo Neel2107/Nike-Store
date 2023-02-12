70d828b287a77e2875903e456535ffcf0e24688f
Neel <neelpatel805804@gmail.com>
2023-02-12 10:56:47 +0530

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,18 +4,18 @@ const Hero = ({heroapi :{title, subtitle, btntext, img, sociallinks, videos}}) =
   return (
     <>
       <div className="relative h-auto w-auto flex flex-col">
-        <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-0 z-10'></div>
+        <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10'></div>
         <div  className=" relative opacity-100 z-20 grid items-center justify-items-center nike-container" >
-          <div className="grid items-center justify-items-center mt-28">
-            <h1>{title}</h1>
-            <h1>{subtitle}</h1>
-            <button className="w-[10vh] h-[8vh] opacity-50">{btntext}</button>
+          <div className="grid items-center justify-items-center mt-28 md:mt-24">
+            <h1 className="text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200 ">{subtitle}</h1>
+            <h1 className="text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200 ">{title}</h1>
+            <button >{btntext}</button>
             {/* <div></div>
             <div></div> */}
             </div>
             <div className="">
             <img src={img}
-            className="w-[55vh]"
+            className=""
             alt="hero-img" />
             </div>
         </div>
