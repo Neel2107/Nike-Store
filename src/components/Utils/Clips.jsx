682ad8bc095459ce9419bf24c2a54b7e51e5868e
Neel <neelpatel805804@gmail.com>
2023-02-13 21:55:49 +0530

    initial commit

--- src/components/Utils/Clips.jsx
+++ src/components/Utils/Clips.jsx
@@ -3,17 +3,16 @@ import React from 'react'
 export const Clips = ({clip, imgsrc}) => {
   return (
     <>
-    <div className='relative h-28  w-32 '>
+    <div className='relative h-28 w-32 '>
         <img 
         src={imgsrc} 
-        alt="img/src"
-        className='' />
+        alt="img/clips"
+        className='inset-0 flex h-full w-full object-cover' />
     </div>
     <video >
         <source 
         type='video/mp4'
         src={clip}
-        className="inset-0 flex h-full w-full object-cover"
         />
        
     </video>
