51d8335271afed7e105f8f0bbfd9024733174806
Neel <neelpatel805804@gmail.com>
2023-02-14 23:21:53 +0530

    video playback

--- src/components/Utils/Clips.jsx
+++ src/components/Utils/Clips.jsx
@@ -9,7 +9,7 @@ export const Clips = ({clip, imgsrc}) => {
         alt="img/clips"
         className='inset-0 flex h-full w-full object-cover absolute top-0 left-0 right-0 rounded-xl opacity-100 z-10 transition-opacity duration-500  ' />
     
-    <div>
+    <div className='bg-white blur-effect-theme absolute top-0 right-0 left-0 opcity-100 z-[100]'>
       <PlayIcon className='icon-style'/>
     </div>
     <video 
