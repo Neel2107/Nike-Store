98bdeb195161c131911962162f03f509af536525
Neel <neelpatel805804@gmail.com>
2023-02-16 19:20:47 +0530

    sales elements

--- src/components/Utils/Item.jsx
+++ src/components/Utils/Item.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const Item = () => {
+const Item = ({id, color, shadow, title, text, img, btn, rating, price}) => {
   return (
-    <div>Item</div>
+    <>
+    <div>
+        <div>
+            <h1>{title}</h1>
+            <p>{text}</p>
+        <div>
+            
+        </div>
+        </div>
+        <div></div>
+    </div>
+    
+    
+    </>
   )
 }
 
