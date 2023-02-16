5dfd5425321d427ad47ae15c6f7c20dfebfec6ee
Neel <neelpatel805804@gmail.com>
2023-02-16 22:50:46 +0530

    bg added to items

--- src/components/Utils/Item.jsx
+++ src/components/Utils/Item.jsx
@@ -4,20 +4,20 @@ import React from "react";
 const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
   return (
     <>
-      <div>
-        <div>
-          <h1>{title}</h1>
-          <p>{text}</p>
-          <div>
-            <div><h1>{price}</h1></div>
-            <div><StarIcon className="icon-style text-slate-900"/><h1>{rating}</h1></div>
+      <div className={` relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center`}>
+        <div className="grid items-center justify-items-center">
+          <h1 className="text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow">{title}</h1>
+          <p className="text-slate-200 font-medium filter drop-shadow">{text}</p>
+          <div className="">
+            <div className=""><h1 className="">{price}</h1></div>
+            <div className=""><StarIcon className="icon-style text-slate-900"/><h1 className="">{rating}</h1></div>
           </div>
           <div>
             <button type="button" className="icon-style text-slate-900"><ShoppingBagIcon/></button>
             <button type="button" className="">{btn}</button>
           </div>
         </div>
-        <div>
+        <div className="">
             <img src={img} alt="img/item-img" className="" />
         </div>
       </div>
