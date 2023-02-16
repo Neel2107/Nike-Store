e6d141ada87aebe2c08a1ea625b33ae0b2674163
Neel <neelpatel805804@gmail.com>
2023-02-16 19:31:56 +0530

    added shoes images

--- src/components/Utils/Item.jsx
+++ src/components/Utils/Item.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import { StarIcon, ShoppingBagIcon } from "@heroicons/react/24/solid";
+import React from "react";
 
-const Item = ({id, color, shadow, title, text, img, btn, rating, price}) => {
+const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
   return (
     <>
-    <div>
+      <div>
         <div>
-            <h1>{title}</h1>
-            <p>{text}</p>
+          <h1>{title}</h1>
+          <p>{text}</p>
+          <div>
+            <div><h1>{price}</h1></div>
+            <div><StarIcon className="icon-style text-slate-900"/><h1>{rating}</h1></div>
+          </div>
+          <div>
+            <button type="button" className="icon-style text-slate-900"><ShoppingBagIcon/></button>
+            <button type="button" className="">{btn}</button>
+          </div>
+        </div>
         <div>
-            
+            <img src={img} alt="img/item-img" className="" />
         </div>
-        </div>
-        <div></div>
-    </div>
-    
-    
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
