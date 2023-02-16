a74910619a963a4ec21ae33e1c395339fb46c8dd
Neel <neelpatel805804@gmail.com>
2023-02-16 22:55:00 +0530

    price bg added

--- src/components/Utils/Item.jsx
+++ src/components/Utils/Item.jsx
@@ -7,17 +7,20 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
       <div className={` relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center`}>
         <div className="grid items-center justify-items-center">
           <h1 className="text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow">{title}</h1>
-          <p className="text-slate-200 font-medium filter drop-shadow">{text}</p>
-          <div className="">
-            <div className=""><h1 className="">{price}</h1></div>
-            <div className=""><StarIcon className="icon-style text-slate-900"/><h1 className="">{rating}</h1></div>
+          <p className="text-slate-200  filter drop-shadow text-base md:text-sm font-normal">{text}</p>
+
+
+
+          <div className=" flex items-center justify-between w-28">
+            <div className=" flex items-center bg-white/80 px-1 rounded"><h1 className="text-black text-sm">${price}</h1></div>
+            <div className=" flex items-center"><StarIcon className="icon-style text-slate-900"/><h1 className="">{rating}</h1></div>
           </div>
-          <div>
+          <div className=" flex items-center">
             <button type="button" className="icon-style text-slate-900"><ShoppingBagIcon/></button>
             <button type="button" className="">{btn}</button>
           </div>
         </div>
-        <div className="">
+        <div className=" flex items-center">
             <img src={img} alt="img/item-img" className="" />
         </div>
       </div>
