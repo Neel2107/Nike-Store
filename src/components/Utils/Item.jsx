884970e05df0afd4d5d931af01e443917123cb35
Neel <neelpatel805804@gmail.com>
2023-02-17 22:50:59 +0530

    added items hover effect

--- src/components/Utils/Item.jsx
+++ src/components/Utils/Item.jsx
@@ -5,7 +5,7 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
   return (
     <>
       <div
-        className={` relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center`}
+        className={` relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center  rounded-xl py-4 px-5 transition-all duration-700 ease-in-out  w-full hover:scale-105 `}
       >
         <div className="grid items-center justify-items-center">
           <h1 className="text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow">
@@ -16,7 +16,7 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
           </p>
 
           <div className=" flex items-center justify-between w-28 my-2  ">
-            <div className=" flex items-center bg-white/80 px-1 rounded">
+            <div className=" flex items-center bg-white/80 px-1 rounded blur-effect-theme">
               <h1 className="text-black text-sm font-medium">${price}</h1>
             </div>
             <div className=" flex items-center gap-1  ">
@@ -30,13 +30,13 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
             <button type="button" className="bg-white 90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200">
               <ShoppingBagIcon className="icon-style text-slate-900" />
             </button>
-            <button type="button" className="bg-white 90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200">
+            <button type="button" className="bg-white 90 blur-effect-theme button-theme px-2 py-1 shadow shadow-sky-200 text-sm text-black ">
               {btn}
             </button>
           </div>
         </div>
         <div className=" flex items-center">
-          <img src={img} alt="img/item-img" className="" />
+          <img src={img} alt="img/item-img" className="h-36 w-34 transitions-theme hover:-rotate-12 " />
         </div>
       </div>
     </>
