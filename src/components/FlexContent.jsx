e3d3c2565d5964aa029c14563b0d31787312ada0
Neel <neelpatel805804@gmail.com>
2023-02-21 19:53:25 +0530

    featured and highlights  animations

--- src/components/FlexContent.jsx
+++ src/components/FlexContent.jsx
@@ -4,7 +4,7 @@ const FlexContent = ({ endpoint : {title, subtitle, text, img, btn,heading, url}
   return (
     <>
     <div className={` flex items-center justify-between lg:flex-col lg:justify-center nike-container`}>
-    <div className='max-w-lg lg:max-w-none w-full md:text-center'>
+    <div className='max-w-lg lg:max-w-none w-full md:text-center grid items-center lg:justify-items-center'>
         <h1 className='text-4xl sm:text-3xl font-bold text-gradient'>{heading}</h1>
         <h1 className='text-5xl lg:text-4xl md:text-3xl sm:text-2xl font-bold  text-slate-900 filter drop-shadow-lg '>{title}</h1>
         <p className='xl:text-sm my-4 text-slate-900 '>{text}</p>
@@ -12,9 +12,9 @@ const FlexContent = ({ endpoint : {title, subtitle, text, img, btn,heading, url}
             <button className='button-theme bg-slate-900 shadow-slate-900 text-slate-100 py-1.5 ' type='button'>{btn}</button>
         </a>
     </div>
-    <div className=''>
+    <div className='flex items-center justify-center max-w-xl relative lg:max-w-none w-full'>
         <img
-        className=''
+        className={`w-auto object-fill transitions-theme`}
          src={img}
          alt={`img/${heading}`}
          />
