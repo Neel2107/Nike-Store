156a015478845d850c15e496c92746a3219043c7
Neel <neelpatel805804@gmail.com>
2023-02-26 21:08:21 +0530

    stories

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Title from "./Utils/Title";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import "@splidejs/react-splide/css"
-import { HeartIcon } from "@heroicons/react/24/solid";
+import {  HashtagIcon, HeartIcon } from "@heroicons/react/24/solid";
+import {ClockIcon} from "@heroicons/react/24/outline"
 const Stories = ({ story: { title, news } }) => {
   return (
     <>
@@ -21,6 +22,11 @@ const Stories = ({ story: { title, news } }) => {
                     <div>
                         <HeartIcon className="icon-style"/><span>{val.like}</span>
                     </div>
+                    <div> <ClockIcon className="icon-style"/><span>{val.time}</span> </div>
+                    <div> <HashtagIcon className="icon-style"/><span>{val.by}</span> </div>
+                </div>
+                <div>
+                  <h1>{val.title}</h1>
                 </div>
               </div>
             </SplideSlide>
